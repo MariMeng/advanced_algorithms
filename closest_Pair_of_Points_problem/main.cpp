7e12c82c4f43decd0665299156650522a88cc469
Mariana <marianabarros.fb@gmail.com>
2026-04-01 21:20:29 -0400

    Function to create points

--- closest_Pair_of_Points_problem/main.cpp
+++ closest_Pair_of_Points_problem/main.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
+struct Ponto{
+    double x, y;
+};
+
+void gerarPontos(vector<Ponto> &pontos, int tam){
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<double> dist(-100.0,100.0);
+
+    for(int i = 0;i<tam;i++){
+        pontos.push_back({dist(gen), dist(gen)});
+    }
+
+}
+
+
 int main(){
 
     cout << "oiii" << endl;
